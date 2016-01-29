6e3695cacff18b1daf93ccd2dc778d17fcd4be2d
Martin Dybdal <dybber@dybber.dk>
2016-01-29 16:32:03 +0100

    Fix copy.c, add host-code for FCL-reverse

--- cases/01_reverse/fcl/reverse.c
+++ cases/01_reverse/fcl/reverse.c
@@ -7,33 +7,40 @@
 #include <sys/time.h>
 #include <mcl.h>
 
-#define BLOCK_DIM 16
 #define NUM_ITERATIONS 100
 
 #define timediff(old, new) (((double)new.tv_sec + 1.0e-6 * (double)new.tv_usec) \
                             - ((double)old.tv_sec + 1.0e-6 * (double)old.tv_usec))
 
-void copy(mclContext ctx,
-               cl_kernel kernel,
-               mclDeviceData input,
-               int size,
-               mclDeviceData output,
-               mclDeviceData sizeOut) {
-    mclSetKernelArg(kernel, 0, sizeof(cl_mem), &input.data);
-    mclSetKernelArg(kernel, 1, sizeof(cl_int), &size);
-    mclSetKernelArg(kernel, 2, sizeof(cl_mem), &output.data);
-    mclSetKernelArg(kernel, 3, sizeof(cl_mem), &sizeOut.data);
-    mclInvokeKernel(ctx, kernel, size, BLOCK_DIM);
+void reverse(mclContext ctx,
+             cl_kernel kernel,
+             int numWgs,
+             int wgsize,
+             mclDeviceData input,
+             int size,
+             mclDeviceData output,
+             mclDeviceData sizeOut) {
+    mclSetKernelArg(kernel, 0, sizeof(cl_int) * wgsize, NULL);
+    mclSetKernelArg(kernel, 1, sizeof(cl_mem), &input.data);
+    mclSetKernelArg(kernel, 2, sizeof(cl_int), &size);
+    mclSetKernelArg(kernel, 3, sizeof(cl_mem), &output.data);
+    mclSetKernelArg(kernel, 4, sizeof(cl_mem), &sizeOut.data);
+    mclInvokeKernel(ctx, kernel, numWgs * wgsize, wgsize);
 }
 
 void test_copy_kernel(mclContext ctx, cl_program p, char* kernelName) {
-    cl_kernel copyKernel = mclCreateKernel(p, kernelName);
+    cl_kernel revKernel = mclCreateKernel(p, kernelName);
 
-    const size_t num_elems = 2048 * 2048;
+    const size_t num_elems = 512;
+
+    int wgsize = 512;
+    int numWgs = (num_elems + wgsize - 1) / wgsize;
 
     int* input = (int*)calloc(num_elems, sizeof(int));
+    int* expected_out = (int*)calloc(num_elems, sizeof(int));
     for (int i = 0; i < num_elems; i++) {
       input[i] = i;
+      expected_out[i] = num_elems-1-i;
     }
 
     mclDeviceData buf = mclDataToDevice(ctx, MCL_R, num_elems, sizeof(int), input);
@@ -41,20 +48,20 @@ void test_copy_kernel(mclContext ctx, cl_program p, char* kernelName) {
     mclDeviceData outlen = mclAllocDevice(ctx, MCL_W, 1, sizeof(int));
 
     // Also serves as warm-up
-    copy(ctx, copyKernel, buf, num_elems, outbuf, outlen);
+    reverse(ctx, revKernel, numWgs, wgsize, buf, num_elems, outbuf, outlen);
     mclFinish(ctx);
 
     cl_int* out = (cl_int*)mclMap(ctx, outbuf, CL_MAP_READ, num_elems * sizeof(cl_int));
 
     cl_int num_errors = 0;
     for (int i = 0; i < num_elems; i++) {
-      if (out[i] != input[i]) {
+      if (out[i] != expected_out[i]) {
         num_errors++;
         if(num_errors > 10) {
           printf("More than 10 errors found. Stopping comparison.\n");
           break;
         } else {
-          printf("Error: out[%d]!=input[%d] (%d, %d)\n", i, i, out[i], input[i]);
+          printf("Error: out[%d]!=expected_out[%d] (%d, %d)\n", i, i, out[i], expected_out[i]);
         }
       }
     }
@@ -68,7 +75,7 @@ void test_copy_kernel(mclContext ctx, cl_program p, char* kernelName) {
       struct timeval begin, end;
       gettimeofday(&begin, NULL);
       for (int i = 0; i < NUM_ITERATIONS; ++i) {
-          copy(ctx, copyKernel, buf, num_elems, outbuf, outlen);
+        reverse(ctx, revKernel, numWgs, wgsize, buf, num_elems, outbuf, outlen);
       }
       mclFinish(ctx);
       gettimeofday(&end, NULL);
@@ -77,20 +84,20 @@ void test_copy_kernel(mclContext ctx, cl_program p, char* kernelName) {
 
       printf("Stats for %s, Throughput = %.4f GB/s, Time = %.5f s, Size = %lu fp32 elements, Workgroup = %u\n", kernelName,
              (1.0e-9 * (double)(num_elems * sizeof(float))/time),
-             time, num_elems, BLOCK_DIM * BLOCK_DIM);
+             time, num_elems, numWgs * wgsize);
 
     }
 
     mclReleaseDeviceData(&buf);
     mclReleaseDeviceData(&outbuf);
-    mclReleaseKernel(copyKernel);
+    mclReleaseKernel(revKernel);
 }
 
 int main () {
   mclContext ctx = mclInitialize(0);
-  cl_program p = mclBuildProgram(ctx, "copy.cl");
+  cl_program p = mclBuildProgram(ctx, "reverse.cl");
 
-  test_copy_kernel(ctx, p, "copy");
+  test_copy_kernel(ctx, p, "reverseGrid512");
 
   mclReleaseProgram(p);
   mclReleaseContext(&ctx);
