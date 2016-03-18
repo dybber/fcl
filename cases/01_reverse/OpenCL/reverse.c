1c3148e91a6c687dba8ee6cd9b0c43a2083e32ec
Martin Dybdal <dybber@dybber.dk>
2016-03-18 15:15:01 +0100

    Fix host-code for reverse

--- cases/01_reverse/OpenCL/reverse.c
+++ cases/01_reverse/OpenCL/reverse.c
@@ -7,39 +7,29 @@
 #include <sys/time.h>
 #include <mcl.h>
 
+#define BLOCK_SIZE 256
 #define NUM_ITERATIONS 1000
 #define timediff(old, new) (((double)new.tv_sec + 1.0e-6 * (double)new.tv_usec) \
                             - ((double)old.tv_sec + 1.0e-6 * (double)old.tv_usec))
 
 void reverse(mclContext ctx,
-             int kernelNo,
              cl_kernel kernel,
              mclDeviceData output,
              mclDeviceData input,
-             int nThreads, int nBlocks) {
+             int num_elems,
+             int useSM) {
 
     mclSetKernelArg(kernel, 0, sizeof(cl_mem), &output.data);
     mclSetKernelArg(kernel, 1, sizeof(cl_mem), &input.data);
-    if (kernelNo != 0) {
+    if (useSM) {
       // dynamic
-      mclSetKernelArg(kernel, 2, sizeof(cl_int)*nThreads, NULL); // local/shared memory
+      mclSetKernelArg(kernel, 2, sizeof(cl_int)*BLOCK_SIZE, NULL); // local/shared memory
     }
 
-    mclInvokeKernel(ctx, kernel, nThreads*nBlocks, nThreads);
+    mclInvokeKernel(ctx, kernel, num_elems, BLOCK_SIZE);
 }
 
-int runReverse(int kernelNo) {
-    const int num_elems = 2048*2048*4;
-
-    char* kernelName;
-    if(kernelNo == 0) {
-      kernelName = "staticReverse";
-    } else {
-      kernelName = "dynamicReverse";
-    }
-
-    mclContext ctx = mclInitialize(0);
-    cl_program p = mclBuildProgram(ctx, "reverse.cl");
+int runReverse(mclContext ctx, cl_program p, char* kernelName, unsigned int num_elems, int useSM) {
     cl_kernel kernel = mclCreateKernel(p, kernelName);
 
     int* input = (int*)calloc(num_elems, sizeof(int));
@@ -50,20 +40,17 @@ int runReverse(int kernelNo) {
     }
 
     /////// Test reverse ///////
-    mclDeviceData buf = mclDataToDevice(ctx, MCL_RW, num_elems, sizeof(int), input);
+    mclDeviceData buf = mclDataToDevice(ctx, MCL_R, num_elems, sizeof(int), input);
     mclDeviceData outbuf = mclAllocDevice(ctx, MCL_W, num_elems, sizeof(int));
 
-    int nThreads = 256;
-    int nBlocks = (num_elems + nThreads - 1)/nThreads;
-
-    printf("Debug: num groups: %d \n", nBlocks);
+    printf("Debug: num groups: %d \n", num_elems/BLOCK_SIZE);
 
     // This also serves as warm-up
-    reverse(ctx, kernelNo, kernel, outbuf, buf, nThreads, nBlocks);
+    reverse(ctx, kernel, outbuf, buf, num_elems, useSM);
     mclFinish(ctx);
 
     // Check results
-    cl_int* out = (cl_int*)mclMap(ctx, outbuf, CL_MAP_READ, sizeof(cl_int));
+    cl_int* out = (cl_int*)mclMap(ctx, outbuf, CL_MAP_READ, num_elems * sizeof(cl_int));
     cl_int num_errors = 0;
     for (int i = 0; i < num_elems; i++) {
       if (out[i] != expected[i]) {
@@ -80,13 +67,13 @@ int runReverse(int kernelNo) {
       printf("PASSED validation. No errors.\n");
     }
 
-    mclUnmap(ctx, buf, out);
+    mclUnmap(ctx, outbuf, out);
 
     // Time 100 calls
     struct timeval begin, end;
     gettimeofday(&begin, NULL);
     for (int i = 0; i < NUM_ITERATIONS; ++i) {
-      reverse(ctx, kernelNo, kernel, outbuf, buf, nThreads, nBlocks);
+      reverse(ctx, kernel, outbuf, buf, num_elems, useSM);
     }
     mclFinish(ctx);
     gettimeofday(&end, NULL);
@@ -95,28 +82,25 @@ int runReverse(int kernelNo) {
     if (num_errors == 0) {
       printf("Stats for %s, Throughput = %.4f GB/s, Time = %.5f s, Size = %u fp32 elements, Workgroup = %u\n", kernelName,
              (1.0e-9 * (double)(num_elems * sizeof(int))/time),
-             time, num_elems, nThreads);
+             time, num_elems, BLOCK_SIZE);
 
     }
 
     mclReleaseDeviceData(&buf);
     mclReleaseKernel(kernel);
-    mclReleaseProgram(p);
-    mclReleaseContext(&ctx);
 
     return 0;
 }
 
-int main(int argc, char* const * argv) {
-  int c;
-  switch(c = getopt(argc, argv, "k:")) {
-  case 'k':
-    runReverse(atoi(optarg));
-    break;
-  default:
-    fprintf(stderr, "err %c\n", c);
-    exit(-1);
-  }
+int main() {
+  int num_elems = 4096*4096;
 
-  return 0;
+  mclContext ctx = mclInitialize(0);
+  cl_program p = mclBuildProgram(ctx, "reverse.cl");
+
+  runReverse(ctx, p, "staticReverse", num_elems, 0);
+  runReverse(ctx, p, "dynamicReverse", num_elems, 1);
+
+  mclReleaseProgram(p);
+  mclReleaseContext(&ctx);
 }
