74f30b8cba65db78ba9e7c6c8caba6cd59314ed4
Martin Dybdal <dybber@dybber.dk>
2016-03-23 10:37:22 +0100

    Forgot to commit these ...

---
+++ src/Language/FCL/Lexer.hs
@@ -0,0 +1,100 @@
+module Language.FCL.Lexer where
+
+import Text.Parsec
+import Text.Parsec.String
+import qualified Text.Parsec.Token as Token
+
+import Data.Functor.Identity (Identity)
+
+reservedNames :: [String]
+reservedNames =
+  ["sig",
+   "fun",
+   "let",
+   "in",
+   "int",
+   "double",
+   "bool",
+   "char",
+   "fn",
+   "true",
+   "false",
+   "kernel",
+   "if",
+   "then",
+   "else"]
+
+reservedOps :: [String]
+reservedOps = []
+
+fclDef :: Token.GenLanguageDef String u Identity
+fclDef = Token.LanguageDef {
+                Token.commentStart     = "(*"
+              , Token.commentEnd       = "*)"
+              , Token.commentLine      = "--"
+              , Token.nestedComments   = True
+              , Token.identStart       = letter
+              , Token.identLetter      = alphaNum <|> char '_'
+              , Token.opStart          = oneOf ""
+              , Token.opLetter         = oneOf ""
+              , Token.reservedOpNames  = reservedOps
+              , Token.reservedNames    = reservedNames
+              , Token.caseSensitive    = True
+  }
+
+lexer :: Token.GenTokenParser String u Identity
+lexer = Token.makeTokenParser fclDef
+
+identifier :: Parser String
+identifier = Token.identifier lexer
+
+reserved :: String -> Parser ()
+reserved   = Token.reserved   lexer
+
+reservedOp :: String -> Parser ()
+reservedOp = Token.reservedOp lexer
+
+stringlit :: Parser String
+stringlit  = Token.stringLiteral lexer
+
+charlit :: Parser Char
+charlit    = Token.charLiteral lexer
+
+parens :: Parser a -> Parser a
+parens     = Token.parens     lexer
+
+brackets :: Parser a -> Parser a
+brackets   = Token.brackets   lexer
+
+angles :: Parser a -> Parser a
+angles     = Token.angles     lexer
+
+braces :: Parser a -> Parser a
+braces     = Token.braces     lexer
+
+integer :: Parser Integer
+integer    = Token.integer    lexer
+
+semi :: Parser String
+semi       = Token.semi       lexer
+
+comma :: Parser String
+comma      = Token.comma      lexer
+
+colon :: Parser String
+colon      = Token.colon      lexer
+
+symbol :: String -> Parser String
+symbol     = Token.symbol     lexer
+
+whitespace :: Parser ()
+whitespace = Token.whiteSpace lexer
+
+decimal :: Parser Integer
+decimal    = Token.decimal    lexer
+
+float :: Parser Double
+float      = Token.float      lexer
+
+lexeme :: Parser a -> Parser a
+lexeme     = Token.lexeme     lexer
---
+++ src/Language/FCL/SourceRegion.hs
@@ -0,0 +1,51 @@
+module Language.FCL.SourceRegion where
+
+import Text.Parsec
+
+data Position = Position { fileName :: FilePath
+                         , line :: Int
+                         , column :: Int }
+                deriving (Eq, Ord)
+
+data Region = Region Position Position
+            | Missing
+  deriving (Eq, Ord)
+
+---------------------
+-- Pretty printing --
+---------------------
+
+printXY :: Position -> String
+printXY p = show (line p) ++ "." ++ show (column p)
+
+instance Show Position where
+  show p = show (fileName p) ++ ":" ++ printXY p
+
+instance Show Region where
+  show (Region p1 p2)
+    | p1 == p2  = show p1
+    | otherwise = show p1 ++ "-" ++ show p2
+  show Missing = "<missing>"
+
+----------------------
+-- Parsec utilities --
+----------------------
+
+-- | Convert Parsec `SourcePos` to `Position`
+fromSourcePos :: SourcePos -> Position
+fromSourcePos pos = Position { fileName = sourceName pos
+                             , line = sourceLine pos
+                             , column = sourceColumn pos }
+
+
+-- | Convert two Parsec `SourcePos` positions to a `Region`
+newRegion :: SourcePos -> SourcePos -> Region
+newRegion pos1 pos2 = Region (fromSourcePos pos1)
+                             (fromSourcePos pos2)
+
+withRegion :: Monad m => ParsecT s u m (Region -> b) -> ParsecT s u m b
+withRegion p = do
+  pos1 <- getPosition
+  f <- p
+  pos2 <- getPosition
+  return (f (newRegion pos1 pos2))
