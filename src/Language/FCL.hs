c081df0cea751fc47ab4cf792d7eea24f29e0d2c
Martin Dybdal <dybber@dybber.dk>
2016-04-27 17:09:25 +0200

    woop

--- src/Language/FCL.hs
+++ src/Language/FCL.hs
@@ -13,7 +13,7 @@ where
 import Language.FCL.Parser      (parseTopLevel, ParseError)
 import Language.FCL.TypeInference (typeinfer, TypeError)
 import Language.FCL.Inline      (inline)
-import Language.FCL.Simplify    (simplify)
+--import Language.FCL.Simplify    (simplify)
 import Language.FCL.Compile     (compileKernels)
 import Language.FCL.Syntax      (Program, Type, Untyped)
 import Language.FCL.PrettyPrint (showType, prettyPrintType, prettyPrintExp, prettyPrintProgram)
