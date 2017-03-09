3f44e39d319edd0b516b2ba9e6f22b85bbf0e88e
Martin Dybdal <dybber@dybber.dk>
2017-03-09 14:36:20 +0100

    -std=c99

--- cases/shared/config.mk
+++ cases/shared/config.mk
@@ -1,5 +1,5 @@
 INCLUDE = -I../../../microcl/ -I../../../include/ -I/usr/local/cuda/include/
 LIBDIR = -L../../../microcl/
-CFLAGS=-Wall -O2 -D_DEBUG
+CFLAGS=-Wall -O2 -D_DEBUG -std=c99
 LIB=-lmcl -lOpenCL
 
