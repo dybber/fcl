cc1eab83a246f5413598ffb92de129839d4ee8e1
Martin Dybdal <dybber@dybber.dk>
2017-03-09 14:37:52 +0100

    library path

--- cases/shared/config.mk
+++ cases/shared/config.mk
@@ -1,5 +1,5 @@
 INCLUDE = -I../../../microcl/ -I../../../include/ -I/usr/local/cuda/include/
-LIBDIR = -L../../../microcl/
+LIBDIR = -L../../../microcl/ -L/usr/local/cuda/lib64/
 CFLAGS=-Wall -O2 -D_DEBUG -std=c99
 LIB=-lmcl -lOpenCL
 
